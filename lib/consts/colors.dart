5ceaeb9e650c736aa24b562028d4560c929e8a16
Alexander <147336344+23yaro@users.noreply.github.com>
2024-03-13 15:14:50 +0300

    BREAKING CHANGE: used new pattern

---
+++ lib/consts/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xBFDA4040);
+const Color tdGreen = Color(0xff7ed679);
+
+const Color tdWhite = Color(0xFFFFFFFF);
+const Color tdGrey = Color(0x26717171);
+const Color tdBlack = Color(0xFF3A3A3A);
+
+const Color tdBGColor = Color(0xFFEEEFF5);
+

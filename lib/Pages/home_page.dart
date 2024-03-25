4d8cafad5c49c3a29678405dad6a86909cbf43e1
Alexander <147336344+23yaro@users.noreply.github.com>
2024-03-25 18:52:18 +0300

    Update home_page.dart

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../block/tsk_bloc.dart';
 import '../consts/colors.dart';
+import '../widgets/add_button_widget.dart';
 import '../widgets/task_widget.dart';
-
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -40,56 +40,31 @@ class _ScrollBarState extends State<ScrollBar> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TskBloc, TskState>(
-        bloc: _tasksListBloc,
-        builder: (context, state) {
-          if (state is TskLoaded) {
-            return CustomScrollView(
-              slivers: <SliverList>[
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: TaskWidget(
-                          key: ValueKey(state.tasksList[index]),
-                          task: state.tasksList[index],
-                          deleteTask: (int id) {},
-                        ),
-                      );
-                    },
-                    childCount: state.tasksList.length,
-                  ),
-                )
-              ],
-            );
-          }
-          return Container();
-        });
-  }
-}
-
-class AddButton extends StatelessWidget {
-  const AddButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: Container(
-        margin: const EdgeInsets.only(right: 10, bottom: 10),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: tdBlack,
-            minimumSize: const Size(50, 60),
-            elevation: 10,
-          ),
-          child: const Icon(
-            Icons.add,
-            color: tdWhite,
-          ),
-          onPressed: () {},
-        ),
-      ),
+      bloc: _tasksListBloc,
+      builder: (context, state) {
+        if (state is TskLoaded) {
+          return CustomScrollView(
+            slivers: <SliverList>[
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: TaskWidget(
+                        key: ValueKey(state.tasksList[index]),
+                        task: state.tasksList[index],
+                        deleteTask: (int id) {},
+                      ),
+                    );
+                  },
+                  childCount: state.tasksList.length,
+                ),
+              )
+            ],
+          );
+        }
+        return Container();
+      },
     );
   }
 }
